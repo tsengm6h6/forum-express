308abbcede677be451ae70949174a4c6135da38b
tyttseng <tyttseng@gmail.com>
2021-03-30 19:06:51 +0800

    add admin restaurants delete feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -60,6 +60,13 @@ const adminController = {
             res.redirect('/admin/restaurants')
           })
       })
+  },
+  deleteRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        restaurant.destroy()
+          .then(() => res.redirect('/admin/restaurants'))
+      })
   }
 }
 
